1c6fb5583ec3d7dd22144157f40b2f30e3b0c148
Sam <stschoberg@gmail.com>
2020-04-17 12:09:34 -0400

    display fields in documents table

--- src/Components/ClassDetailsTable.js
+++ src/Components/ClassDetailsTable.js
@@ -1,38 +1,56 @@
 import React from 'react'
 import { Table, Button } from 'semantic-ui-react'
 import _ from 'lodash'
-import { NavLink } from 'react-router-dom'
 import { Storage, graphqlOperation, API } from 'aws-amplify';
 import * as Queries from '../Queries'
-import Async from 'react-promise'
 
 
 
 class ClassDetailsTable extends React.Component{
-
-    state = {
+    constructor(props){
+      super(props)
+    
+    this.state = {
         column: null,
         data: [],
+        metaData: {},
         direction: null,
         metaData: {
 
         }
-      }
 
-    componentDidMount () {
-      console.log("SHIT")
-        this.setState({data: this.getExams()})
-        console.log("STATE")
-        console.log(this.state.data)
+      }
+      this.getMetaData = this.getMetaData.bind(this)
+      this.getExams = this.getExams.bind(this)
+      this.componentDidMount = this.componentDidMount.bind(this)
     }
 
+    componentDidMount () {
+      this.getExams().then(exams => this.doStuff(exams))
 
+
+
+    }
+
+    doStuff = exams => {
+      this.setState({data: exams});
+      this.getMetaData().then(x => Promise.all(x).then(z => this.setState({metaData: z})))
+    }
+
+    getMetaData = async () => {
+              const q = _.map(this.state.data, async fileName => await Storage.get(`uploads/${fileName}`, {download: true})
+              .then(x => x.Metadata).catch(err=>"NONE"))
+
+
+              return q
+    }
 
      getExams = async () => {
-        const d =  await API.graphql(graphqlOperation(Queries.getClassDetails(this.props.classID))).then(res => res.data.getClass.exams)
-        const q = _.map(d, async fileName => await Storage.get(`uploads/${fileName}`, {download: true})
-              .then(x => x.Metadata).catch(x=>"NONE"))
-        return q
+        const d =  await API.graphql(graphqlOperation(
+          Queries.getClassDetails(this.props.classID)))
+          .then(data =>(data.data.getClass.exams)) 
+
+        return d
     }
 
       handleSort = (clickedColumn) => () => {
@@ -62,7 +80,8 @@ class ClassDetailsTable extends React.Component{
           console.log(link)
       }
     render(){ 
-        console.log(this.state.data.length)
+      console.log("YOYOYOY")
+      console.log(this.state)
         const { column, data, direction } = this.state
         // const z = exams.then(x => console.log(x))
         // console.log("Z")
@@ -94,16 +113,18 @@ class ClassDetailsTable extends React.Component{
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {_.map(this.state.data, id => (
+          {_.map(this.state.metaData, id => (
             //  console.log("SHIT")
             <Table.Row key={id}>
               <Table.Cell>
               <Button onClick={() => this.handleDownload(id)} animated='fade'>
-                <Button.Content visible>{id.documentName}</Button.Content>
+                <Button.Content visible>{id.documentname}</Button.Content>
                 <Button.Content hidden>Download</Button.Content>
                 </Button>            
               </Table.Cell>
-              <Table.Cell>{id}</Table.Cell>
+              <Table.Cell>{id.semester}</Table.Cell>
+              <Table.Cell>{id.professor}</Table.Cell>
+
             </Table.Row>
           ))}
         </Table.Body>
